b8d4687cff40d00ac4f3c96e5a2c6413d8e24368
ichynul <ichynul@outlook.com>
2020-01-30 13:53:13 +0800

    update

--- src/admin/controller/Index.php
+++ src/admin/controller/Index.php
@@ -25,10 +25,20 @@ class Index extends Controller
     {
         $builder = new Builder();
 
-        $row = $builder->row();
+        $form = $builder->form();
 
-        $row->column(6)->form();
-        $row->column(6)->table();
+        $form->text('name', '姓名', 6, 'hell', 'width="1000"')->value('小明')->size([12, 12])->error('出错了');
+        $form->text('phone', '电话', 6)->value('1234500006789')->help('帮助信息')->error('666');
+        $form->textarea('note', '备注', 6)->value('大家好！')->errorClass('has-error');
+        $form->html('')->value('<p style="color:red;">hello world !</p>');
+        $form->divider('test', '', 6)->value('it ok');
+        $form->raw('notice', '注意', 6)->value('<span style="color:green;">嘿</span>')->labelClass('nihao')->labelAttr('width="200"');
+        $form->checkbox('hobi', '爱好', 6)->options([
+            '1' => '游泳',
+            '2' => '爬山',
+        ])->inline(true)->checkAllBtn()->checked([1]);
+
+        //$row->column(6)->table();
 
         return $builder->render();
     }
