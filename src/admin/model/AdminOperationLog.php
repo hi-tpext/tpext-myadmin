773de26d00497094adf178fcd2c8dff6afd8f511
LHY <ichynul@outlook.com>
2020-03-27 16:38:38 +0800

    fix

--- src/admin/model/AdminOperationLog.php
+++ src/admin/model/AdminOperationLog.php
@@ -8,29 +8,17 @@ class AdminOperationLog extends Model
 {
     protected $autoWriteTimestamp = 'dateTime';
 
-    protected $user;
-
     public function getNameAttr($value, $data)
     {
-        $user = $this->getUser($data);
+        $user = AdminUser::get($data['user_id']);
 
         return $user ? $user['name'] : '';
     }
 
     public function getUsernameAttr($value, $data)
     {
-        $user = $this->getUser($data);
+        $user = AdminUser::get($data['user_id']);
 
         return $user ? $user['username'] : '';
-    }
-
-    private function getUser($data)
-    {
-        if ($this->user) {
-            return $this->user;
-        }
-
-        $this->user = AdminUser::get($data['user_id']);
-        return $this->user;
     }
 }
