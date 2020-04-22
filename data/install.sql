c8d60c797c53618cb01bfe1ca414c725cbe5f848
ichynul <ichynul@outlook.com>
2020-04-22 15:58:09 +0800

    update sql

--- data/install.sql
+++ data/install.sql
@@ -121,15 +121,15 @@ INSERT INTO `__PREFIX__admin_user` (`id`, `role_id`, `username`, `password`, `sa
 (1, 1, 'admin', '0796647d241a5014670013b324a155ee', '15fb7db', '管理员', '/assets/lightyearadmin/images/no-avatar.jpg', '', '', 0, 1, '', 1, '2020-01-01 00:00:00', '2020-03-01 20:19:11', '2020-03-08 23:37:36');
 
 -- 菜单
-INSERT INTO `__PREFIX__admin_menu` (`id`, `parent_id`, `sort`, `title`, `url`, `icon`, `create_time`, `update_time`) VALUES
-(1, 0, 1, '首页', '/admin/index/welcome', 'mdi mdi-home', '2020-03-03 20:26:49', '2020-03-03 20:26:49'),
-(2, 0, 98, '权限管理', '#', 'mdi mdi-account-check', '2020-03-03 20:27:14', '2020-03-03 20:27:14'),
-(3, 2, 1, '菜单管理', '/admin/menu/index', 'mdi mdi-arrange-send-to-back', '2020-03-03 20:27:49', '2020-03-03 20:34:40'),
-(4, 2, 2, '权限设置', '/admin/permission/index', 'mdi mdi-account-key', '2020-03-03 20:28:35', '2020-03-03 20:28:35'),
-(5, 2, 3, '管理员', '/admin/admin/index', 'mdi mdi-account-card-details', '2020-03-03 20:29:07', '2020-03-03 20:34:25'),
-(6, 2, 4, '角色管理', '/admin/role/index', 'mdi mdi-account-multiple', '2020-03-03 20:31:22', '2020-03-03 20:31:22'),
-(7, 2, 5, '用户分组', '/admin/group/index', 'mdi mdi-account-network', '2020-03-03 20:31:22', '2020-03-03 20:31:22'),
-(8, 2, 6, '操作记录', '/admin/operationlog/index', 'mdi mdi-playlist-check', '2020-03-03 20:32:06', '2020-03-03 20:32:06'),
-(9, 0, 99, '系统管理', '#', 'mdi mdi-settings', '2020-03-03 20:35:11', '2020-03-03 20:35:11'),
-(10, 9, 1, '扩展管理', '/admin/extension/index', 'mdi mdi-puzzle', '2020-03-03 20:36:54', '2020-03-03 20:36:54'),
-(11, 9, 2, '平台设置', '/admin/config/index', 'mdi mdi-settings-box', '2020-03-03 20:37:29', '2020-03-03 20:37:29');
+INSERT INTO `__PREFIX__admin_menu` (`id`, `parent_id`, `sort`, `title`, `url`, `icon`, `enable`, `create_time`, `update_time`) VALUES
+(1, 0, 1, '首页', '/admin/index/welcome', 'mdi mdi-home', 1, '2020-03-03 20:26:49', '2020-03-03 20:26:49'),
+(2, 0, 98, '权限管理', '#', 'mdi mdi-account-check', 1, '2020-03-03 20:27:14', '2020-03-03 20:27:14'),
+(3, 2, 1, '菜单管理', '/admin/menu/index', 'mdi mdi-arrange-send-to-back', 1, '2020-03-03 20:27:49', '2020-03-03 20:34:40'),
+(4, 2, 2, '权限设置', '/admin/permission/index', 'mdi mdi-account-key', 1, '2020-03-03 20:28:35', '2020-03-03 20:28:35'),
+(5, 2, 3, '管理员', '/admin/admin/index', 'mdi mdi-account-card-details', 1, '2020-03-03 20:29:07', '2020-03-03 20:34:25'),
+(6, 2, 4, '角色管理', '/admin/role/index', 'mdi mdi-account-multiple', 1, '2020-03-03 20:31:22', '2020-03-03 20:31:22'),
+(7, 2, 5, '用户分组', '/admin/group/index', 'mdi mdi-account-network', 1, '2020-03-03 20:31:22', '2020-03-03 20:31:22'),
+(8, 2, 6, '操作记录', '/admin/operationlog/index', 'mdi mdi-playlist-check', 1, '2020-03-03 20:32:06', '2020-03-03 20:32:06'),
+(9, 0, 99, '系统管理', '#', 'mdi mdi-settings', 1, '2020-03-03 20:35:11', '2020-03-03 20:35:11'),
+(10, 9, 1, '扩展管理', '/admin/extension/index', 'mdi mdi-puzzle', 1, '2020-03-03 20:36:54', '2020-03-03 20:36:54'),
+(11, 9, 2, '平台设置', '/admin/config/index', 'mdi mdi-settings-box', 1, '2020-03-03 20:37:29', '2020-03-03 20:37:29');
