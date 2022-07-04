70f12643217142fa6f1a20d928d99b37af8d9f6c
ichynul <ichynul@163.com>
2022-07-04 11:53:53 +0800

    fix

--- src/webman/route.php
+++ src/webman/route.php
@@ -3,8 +3,8 @@
 use Webman\Route;
 
 Route::group('/admin', function () {
-    Route::any('', [tpext\myadmin\admin\controller::class, 'index']);
-    Route::any('/', [tpext\myadmin\admin\controller::class, 'index']);
-    Route::any('/index', [tpext\myadmin\admin\controller::class, 'index']);
-    Route::any('/index/index', [tpext\myadmin\admin\controller::class, 'index']);
+    Route::any('', [tpext\myadmin\admin\controller\Index::class, 'index']);
+    Route::any('/', [tpext\myadmin\admin\controller\Index::class, 'index']);
+    Route::any('/index', [tpext\myadmin\admin\controller\Index::class, 'index']);
+    Route::any('/index/index', [tpext\myadmin\admin\controller\Index::class, 'index']);
 });
