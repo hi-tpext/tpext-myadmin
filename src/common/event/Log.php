50414902596f9e6928dbf8641ab9c0d17dc3a2dc
ichynul <ichynul@163.com>
2023-03-10 18:17:07 +0800

    日志字段

--- src/common/event/Log.php
+++ src/common/event/Log.php
@@ -54,13 +54,37 @@ class Log
 
         unset($param['password'], $param['__table__'], $param['__search__'], $param['__token__']);
 
+        $path = implode('/', ['admin', $controller, $action]);
+
+        $operation_log_fields_except = $config['operation_log_fields_except'] ?? '';
+
+        if ($operation_log_fields_except) {
+            $rules = explode("\n", $operation_log_fields_except);
+            foreach ($rules as $rule) {
+                if (empty($rule)) {
+                    continue;
+                }
+                $ruleArr = explode(':', trim($rule));
+                if (count($ruleArr) != 2) {
+                    continue;
+                }
+                $rulePaths = explode(',', trim($ruleArr[0]));
+                if ($rulePaths[0] == '*' || in_array($path, $rulePaths)) {
+                    $fields = explode(',', $ruleArr[1]);
+                    foreach ($fields as $f) {
+                        unset($param[trim($f)]);
+                    }
+                }
+            }
+        }
+
         if (in_array($method, ['PATCH', 'DELETE'])) {
             unset($param[strtolower($method)]);
         }
 
         AdminOperationLog::create([
             'user_id' => $admin_id,
-            'path' => implode('/', ['admin', $controller, $action]),
+            'path' => $path,
             'method' => request()->method(),
             'ip' => request()->ip(),
             'data' => json_encode($param, JSON_UNESCAPED_UNICODE),
--- src/config.php
+++ src/config.php
@@ -20,6 +20,7 @@ return [
     'admin_group_title' => '分组',
     'admin_group_model' => '',
     'operation_log_catch' => ['POST', 'PUT', 'PATCH', 'DELETE'],
+    'operation_log_fields_except' => '*:content',
     'index_page_style' => '',
     'login_page_style' => '1',
     'login_page_view_path' => '',
@@ -38,15 +39,16 @@ return [
             $form->text('admin_group_title', '管理员分组名称')->help('如:`部门，分店');
             $form->text('admin_group_model', '管理员分组模型')->help('如:`\tpext\myadmin\admin\model\AdminGroup`，你可以自己实现分组。树形结构需要配合\tpext\builder\traits\TreeModel');
             $form->checkbox('operation_log_catch', '操作日志记录类型')->options(['GET' => 'GET', 'POST' => 'POST', 'PUT' => 'PUT', 'PATCH' => 'PATCH', 'DELETE' => 'DELETE']);
+            $form->textarea('operation_log_fields_except', '记录日志排除字段')->help("避免记录一些内容很长但意义不大的字段到操作日志里面，如文章内容、产品详情等。规则`path:fields`,path可用`*`代表所有，多个规则用换行分割。例如:<pre>*:content\nadmin/shopgoods/edit:description</pre>");
             $form->select('index_page_style', 'index主体页面风格')->options(Module::getInstance()->getIndexViews());
             $form->select('login_page_style', '登录页面风格')->options(Module::getInstance()->getLoginViews());
-            $form->radio('index_top_menu', '顶部菜单')->options([0 => '否', 1 => '是'])->help('菜单第一层放在页面顶部，点击切换左侧菜单');
         });
 
         $form->right(8)->with(function () use ($form) {
             $form->text('name', '名称');
             $form->textarea('description', '描述')->rows(2);
             $form->textarea('logo', '左上角Logo')->rows(2);
+            $form->radio('index_top_menu', '顶部菜单')->options([0 => '否', 1 => '是'])->help('菜单第一层放在页面顶部，点击切换左侧菜单');
             $form->textarea('copyright', '底部版权')->rows(2);
             $form->image('favicon', 'Favicon图片');
             $form->image('login_logo', '登录页面Logo');
