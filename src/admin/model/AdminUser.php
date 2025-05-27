3608621bda857e332c53f40a80002fc5d48c2f10
ichynul <ichynul@163.com>
2025-05-27 13:17:09 +0800

    fix: 深层控制器权限问题

--- src/admin/model/AdminUser.php
+++ src/admin/model/AdminUser.php
@@ -140,7 +140,6 @@ class AdminUser extends Model implements Auth
     public static function checkUrl($url, $user = null)
     {
         $url = preg_replace('/(\.html)?\?.*$/i', '', $url);
-        $url = str_replace('.', '/', $url);
 
         if (!Module::isInstalled()) {
             if (preg_match('/^\/admin\/extension\/\w+/i', $url)) {
