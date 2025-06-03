b11800b8bcac32d125f6488b64e169d92b891cfb
ichynul <ichynul@163.com>
2025-06-03 16:20:12 +0800

    fix 权限检测问题

--- src/admin/model/AdminUser.php
+++ src/admin/model/AdminUser.php
@@ -130,7 +130,8 @@ class AdminUser extends Model implements Auth
      */
     public static function checkUrl($url, $user = null)
     {
-        $url = preg_replace('/(\.html)?\?.*$/i', '', $url);
+        $url = preg_replace('/\.html/i', '', $url);
+        $url = preg_replace('/\?.*$/i', '', $url);
 
         if (!Module::isInstalled()) {
             if (preg_match('/^\/admin\/extension\/\w+/i', $url)) {
