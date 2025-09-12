3393c9a6f076c5fa0fe5ad2b37b7471eb2503dad
ichynul <ichynul@163.com>
2025-09-12 13:05:00 +0800

    url调整

--- src/admin/model/AdminUser.php
+++ src/admin/model/AdminUser.php
@@ -2,7 +2,6 @@
 
 namespace tpext\myadmin\admin\model;
 
-use think\helper\Str;
 use think\Model;
 use tpext\builder\inface\Auth;
 use tpext\myadmin\common\Module;
@@ -99,7 +98,6 @@ class AdminUser extends Model implements Auth
      */
     public function checkPermission($admin_id, $controller, $action)
     {
-        $controller = Str::studly($controller);
         $user = static::find($admin_id);
 
         if (!$user) {
@@ -150,13 +148,7 @@ class AdminUser extends Model implements Auth
             return true;
         }
 
-        $path = explode('/', trim($url, '/'));
-
-        if (count($path) < 3) {
-            return false;
-        }
-
-        $url = implode('/', ['', $path[0], Str::snake($path[1]), strtolower($path[2])]);
+        $url = trim($url, '/');
 
         $noNeed = [
             '/admin/index/index', '/admin/index/captcha', '/admin/index/welcome', '/admin/index/denied',
@@ -174,11 +166,6 @@ class AdminUser extends Model implements Auth
         }
 
         $prmission = AdminPermission::where(['url' => $url])->find();
-
-        if (!$prmission && count($path) > 3) {
-            $url = implode('/', ['', Str::snake($path[0] . '/' . $path[1]), $path[2], $path[3]]);
-            $prmission = AdminPermission::where(['url' => $url])->find();
-        }
 
         if (!$prmission) {
             return false;
