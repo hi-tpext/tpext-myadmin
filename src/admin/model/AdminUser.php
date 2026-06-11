c7b1a86a36c7d62b32983609091b449aad7b2f02
ichynul <ichynul@163.com>
2026-06-11 13:21:30 +0800

    使用password_hash验证密码

--- src/admin/model/AdminUser.php
+++ src/admin/model/AdminUser.php
@@ -74,11 +74,14 @@ class AdminUser extends Model implements Auth
      */
     public function passCrypt($pwd)
     {
-        $pwd = md5($pwd);
-        $salt = substr(md5(time()), mt_rand(0, 22), 10);
-        $pwd = md5($salt . $pwd . $salt);
+        $hash = password_hash($pwd, PASSWORD_BCRYPT, ['cost' => 12]);
 
-        return [$pwd, $salt];
+        return [$hash, 'pwd_hash'];
+        // $pwd = md5($pwd);
+        // $salt = substr(md5(time()), mt_rand(0, 22), 10);
+        // $pwd = md5($salt . $pwd . $salt);
+
+        // return [$pwd, $salt];
     }
 
     /**
@@ -91,6 +94,10 @@ class AdminUser extends Model implements Auth
      */
     public function passValidate($savedCryptPwd, $savedSalt, $inputPwd)
     {
+        if ($savedSalt == 'pwd_hash') {
+            return password_verify($inputPwd, $savedCryptPwd);
+        }
+
         $inputPwd = md5($inputPwd);
 
         return $savedCryptPwd == md5($savedSalt . $inputPwd . $savedSalt);
